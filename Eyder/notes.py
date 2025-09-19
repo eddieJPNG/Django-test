f32da476f192a69068189ce4e4233bf284b8b617
Edson <rochaedsonn12@gmail.com>
2025-09-19 17:25:26 -0300

    More info in notes.

--- Eyder/notes.py
+++ Eyder/notes.py
@@ -1,12 +1,21 @@
-def oi():
-    print('oi')
+# def oi():
+#     print('oi')
     
-    return
+#     return
 
-oi()
+# oi()
 
-print(bin(64000009))
+# print(bin(64000009))
 
-x = bin(64000009)
-print(x)
-print(x.count('1'))
+# x = bin(64000009)
+# print(x)
+# print(x.count('1'))
+
+#VALORES DEFAULT PARA FUNCOES
+
+
+def dados(name='nome', age=18, height=1.80):
+    print(f'Nome: {name}, Idade: {age}, Altura: {height}')
+
+dados('Eddie', 20, 1.75)
+
