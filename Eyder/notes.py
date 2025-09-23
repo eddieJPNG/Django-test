7c0ccc51fbbfdc80a684e24e4390cdb651f2feba
Edson <rochaedsonn12@gmail.com>
2025-09-23 17:42:44 -0300

    More phyton's university lessons.

--- Eyder/notes.py
+++ Eyder/notes.py
@@ -24,6 +24,56 @@ def soma(a,b):
     return c
     
 
-# soma = soma(2,3)
-# print(soma)
+soma = soma(2,3)
+print(soma)
+
+def avg(values):
+    if not isinstance(values, (list, tuple)):
+        return 0.0
+    
+    if isinstance(values, (int, float)):
+        return values
+    
+    count = len(values)
+    if count == 0:
+        return 0.0
+
+    sum = 0.0
+    for value in values:
+        sum += value # sum = sum + value
+    return sum / len(values)
+
+print(avg([1,4,50]))
+print(avg((2,45,22)))
+print(avg(20))
+print(avg(2.666))
+print(avg(5.01))
+
+def avg2(*values):
+    return print(type(values), values)
+
+#O "*" transforma os argumentos em uma tupla
+
+print(avg2(12,45,67))
+
+#isinstance
+#FERRAMENTA MUITO PODEROSA!
+
+
+#NOS PARAMETROS DA FUNCOES UM "*" ANTES DO PARAMETRO TRANSFORMA OS ARGUMENTOS EM UMA TUPLA, IDEPENDENTEMENTE DA SITUAÇÃO É O DIABO DE UMA TUPLA.
+
+# Grupo dos bonecos: 8 horas;
+# Grupo dos arquitetos: 4 horas;
+# Grupo dos musicos: 6 horas;
+# Grupo dos desenhistas: 12 horas.
+
+# Aradhel bonecos 10 - 1
+# Aerin arquitetos 15 - 3
+# Anna musicos 10 - 1
+# Elbereth musicos 10 - 1
+# Freda desenhistas 15 - 1
+# Arwen bonecos 10 - 1
+# Logolas bonecos 10 - 1
+
+print(1+3+1+1+1+1+1)
 
--- teste.js
+++ teste.js
@@ -1,3 +1,3 @@
-let n = 200;
-
-console.log(n);
+const num = 50;
+let rim = num = 60; // SyntaxError: Identifier 'num' has already been declared
+console.log(num);
